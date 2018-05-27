404beb2340657d9c53d861dfd3be2a916627bd94
Olivier Verwoerd <olivier.verwoerd@student.hu.nl>
2018-05-27 13:58:07 +0200

    Added minor checks

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "wrap-hwlib.hpp"
-#include "MovingPlatform.hpp"
+#include "moving-platform.hpp"
 
 int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
 
     hwlib::wait_ms(1000);
+    Platform test;
+    int x = test.getOffset();
     hwlib::cout << "Hello world!" << hwlib::endl;
-
+    hwlib::cout << x << hwlib::endl;
     return 0;
 }
