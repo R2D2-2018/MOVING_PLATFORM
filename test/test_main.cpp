b2dd2e7110abee8a92a9ad85f3724c3a2a48eea6
Daniel van Vliet <Danielvvliet@hotmail.com>
2018-06-12 14:46:20 +0200

    updated test_main.cpp

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -4,16 +4,12 @@
 
 TEST_CASE("Get and set") {
     MovingPlatform::Platform test;
-    test.setOffset(10);
     test.setSmoothing(10);
     test.setWheelSize(10);
-    REQUIRE(test.getOffset() == 10);
     REQUIRE(test.getSmoothing() == 10);
     REQUIRE(test.getWheelSize() == 10);
-    test.setOffset(2);
     test.setSmoothing(2);
     test.setWheelSize(2);
-    REQUIRE(test.getOffset() == 2);
     REQUIRE(test.getSmoothing() == 2);
     REQUIRE(test.getWheelSize() == 2);
 }
