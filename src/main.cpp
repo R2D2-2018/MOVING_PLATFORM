ff1abf03c4a1f751db2083a89d584dec1378e30c
jasper <Jasper.Smienk@student.hu.nl>
2018-06-22 15:24:08 +0200

    Removed redundant lines in .gitignore. Removed hello world from main. Replaced stdint.h with hwlib.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main() {
 
     hwlib::wait_ms(1000);
     MovingPlatform::Platform test;
-    hwlib::cout << "Hello world!" << test.getWheelSize() << hwlib::endl;
+    hwlib::cout << test.getWheelSize() << hwlib::endl;
 
     return 0;
 }
